0f707c3b901c70dd5df34f5577555ddc89dbc06a
Toby Versteeg <toby@codegorilla.nl>
2021-05-18 11:15:44 +0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+use App\Libraries\View;
+use App\Libraries\MySql;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a user record into the database
+     */
+    public function store()
+    {
+        
+    }
+
+    /**
+     * Updates a user record into the database
+     */
+    public function update()
+    {
+        $data = [
+            'first_name' => "Code",
+            'last_name' => "Gorilla",
+            'email' => "toby@codegorilla.nl",
+            'password' => "q3234234q3213^^!^w23123",
+            'birthday' => "1990-12-31",
+            'created' => date("Y-m-d H:i:s"),
+        ];
+
+        UserModel::update($data, 1);
+    }
+
+    /**
+     * Archive a user record into the database
+     */
+    public function destroy(int $id)
+    {
+
+    }
+
+}
+
